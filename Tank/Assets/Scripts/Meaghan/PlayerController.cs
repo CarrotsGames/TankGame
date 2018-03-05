aca776a606851b29c0554031c084a45f11c70887
ResidentUnleashed <31601762+ResidentUnleashed@users.noreply.github.com>
2018-03-05 11:40:52 +1100

    Added scripts folder

---
+++ Tank/Assets/Scripts/Meaghan/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Variables
+    [Header("Alter Movement")]
+    [SerializeField]
+    private float speed = 3.0f;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    private void Movement()
+    {
+       
+    }
+}
